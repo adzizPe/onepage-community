615cd797a9f07c7aef6f82f0e57d3c7161210bda
Adzandyma AL Aziz <adzanaziz2@gmail.com>
2024-05-31 00:52:50 +0700

    date

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,3 +180,18 @@ sr.reveal(`.home__data, .home__img,
             .footer__content`, {
     interval: 200
 })
+
+// date days
+document.getElementById('date').innerHTML = new Date().getFullYear()
+
+let currentDate = new Date();
+let day = currentDate.getDate();
+let month = currentDate.toLocaleString('default', { month: 'long' });
+let year = currentDate.getFullYear();
+let formattedDate = currentDate.toLocaleDateString('id-ID', { day: 'numeric', month: 'long', year: 'numeric' });
+document.getElementById('header-date').innerHTML = formattedDate;
+
+let week = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+let dayNumber = new Date().getDay();
+let dayName = week[dayNumber];
+document.getElementById('days').innerHTML = dayName;
